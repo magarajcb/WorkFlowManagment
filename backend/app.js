e94afeb83875c4cc87b04bdf3411e53b7fb24b9c
magaraj <magarajcb@gmail.com>
2026-06-22 20:26:19 +0530

    login and reg done

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express=require("express")
 const cors=require("cors")
+const authRoutes=require("./routes/authRoutes")
 const app=express()
 app.use(cors())
 app.use(express.json())
+app.use("/api/auth",authRoutes)
 app.get("/",(req,res)=>{
     res.send("Workflow app running")
 })
+
 module.exports=app;
