206b7302f3f1dcf5052e581d7b8555b1c3bd5f71
magaraj <magarajcb@gmail.com>
2026-06-23 20:16:24 +0530

    project added

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,5 +1,68 @@
+const Project = require("../models/Project")
+
 const createProject= async(req,res)=>{
     try{
         const{title,description}=req.body
+        const project= await Project.create({
+            title,
+            description,
+            owner:req.userId
+        })
+        res.status(201).json(project)
+    }catch(error){
+        res.status(500).json({
+            message:error.message
+        })
     }
 }
+const getProjects= async(req,res)=>{
+    try{
+        const projects=await Project.find({
+            owner:req.userId
+    })
+    res.status(200).json(projects)
+}catch(error){
+    res.status(500).json({
+        message:error.message
+    })
+}}
+const getProjectById=async(req,res)=>{
+    try{
+        const project=await Project.findById(req.params.id)
+        if(!project){
+            return res.status(404).json({
+                message:"Project not found"
+            })
+                   }
+                    res.status(200).json(project)
+    }catch(error){
+        res.status(500).json({
+            message:error.message
+        })
+    }
+}
+const deleteProject=async(req,res)=>{
+    try{
+        const project=await Project.findById(req.params.id)
+        if(!project){
+            return res.status(404).json({
+                message:"Project Not Found"
+            })
+                    }
+                    await project.deleteOne()
+                    res.status(200).json({
+                message:"Project deleted succesfully"
+            })
+    }
+    catch(error){
+        res.status(500).json({
+            message:error.message
+        })
+    }
+}
+module.exports = {
+  createProject,
+  getProjects,
+  getProjectById,
+  deleteProject,
+};
